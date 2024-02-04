1c508d34905c154d1e8f7cc9a043bfa0a194e453
Utpal Tiwari <utpaltiwari02@gmail.com>
2024-02-04 19:54:09 +0530

    Third Commit

--- lib/game_screen.dart
+++ lib/game_screen.dart
@@ -1,17 +1,133 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/material.dart';
 
 class GameScreen extends StatefulWidget {
   String player1;
   String player2;
   GameScreen({required this.player1, required this.player2});
-
   @override
   State<GameScreen> createState() => _GameScreenState();
 }
 
 class _GameScreenState extends State<GameScreen> {
+  late List<List<String>> _board;
+  late String _currentPlayer;
+  late String _winner;
+  late bool _gameOver;
+  @override
+  void initState() {
+    super.initState();
+    _board = List.generate(3, (_) => List.generate(3, (_) => ""));
+    _currentPlayer = "X";
+    _winner = "";
+    _gameOver = false;
+  }
+
+  void _resetGame() {
+    setState(() {
+      _board = List.generate(3, (_) => List.generate(3, (_) => ""));
+      _currentPlayer = "X";
+      _winner = "";
+      _gameOver = false;
+    });
+  }
+
+  void _makeMove(int row, int col) {
+    if (_board[row][col] != "" || _gameOver) {
+      return;
+    }
+    setState(() {
+      _board[row][col] = _currentPlayer;
+      //check for WINNER !
+      if (_board[row][0] == _currentPlayer &&
+          _board[row][1] == _currentPlayer &&
+          _board[row][2] == _currentPlayer) {
+        _winner = _currentPlayer;
+        _gameOver = true;
+      } else if (_board[0][col] == _currentPlayer &&
+          _board[1][col] == _currentPlayer &&
+          _board[2][col] == _currentPlayer) {
+        _winner = _currentPlayer;
+        _gameOver = true;
+      } else if (_board[0][0] == _currentPlayer &&
+          _board[1][1] == _currentPlayer &&
+          _board[2][2] == _currentPlayer) {
+        _winner = _currentPlayer;
+        _gameOver = true;
+      } else if (_board[0][2] == _currentPlayer &&
+          _board[1][1] == _currentPlayer &&
+          _board[2][0] == _currentPlayer) {
+        _winner = _currentPlayer;
+        _gameOver = true;
+      }
+      //switch players
+      _currentPlayer = _currentPlayer == "X" ? "O" : "X";
+
+      //check for a tie
+      if (!_board.any((row) => row.any((cell) => cell == ""))) {
+        _gameOver = true;
+        _winner = "It's a Tie";
+      }
+
+      if (_winner != "") {
+        AwesomeDialog(
+          context: context,
+          dialogType: DialogType.success,
+          animType: AnimType.rightSlide,
+          btnOkText: "Play Again",
+          title: _winner == "X"
+              ? widget.player1 + "Won!"
+              : _winner == "O"
+                  ? widget.player2 + "Won!"
+                  : "It's a Tie",
+          btnOkOnPress: () {
+            _resetGame();
+          },
+        ).show();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        backgroundColor: Color.fromARGB(180, 24, 88, 172),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 70,
+              ),
+              SizedBox(
+                height: 120,
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Turn: ",
+                          style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                        Text(
+                          _currentPlayer == "X"
+                              ? widget.player1 + " ($_currentPlayer)"
+                              : widget.player2 + " ($_currentPlayer)",
+                          style: TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
